49db26d04f3c266ae7c34ae2791232149f359e71
Trey Tiderman <tiderman.trey@gmail.com>
2024-01-08 17:41:42 -0600

    seperate App.svelte into its components

--- src/App.svelte
+++ src/App.svelte
@@ -1,104 +1,31 @@
 <script>
-    // Imports
-    import { onMount } from "svelte";
-    import { api, user_v1, logger_v0 } from "./js/api";
-    import { state } from "./js/state.js";
+    import { volatile } from "./js/global-volatile-store.js";
 
     // Components
-    import Login from "./pages/Login.svelte";
-    import Tabs from "./pages/WindowTabs.svelte";
-    import Split from "./pages/WindowSplit.svelte";
-    import Modal from "./components/Modal.svelte";
-
-    // Variables
-    // let url = "wss://2.trey.app";
-    let url = "ws://192.168.1.1:4620";
-    let status =
-        localStorage.getItem("status") === "close" ? "close" : "startup";
-    let lostConnection = localStorage.getItem("lost-connection");
-
-    // Startup
-    onMount(async () => {
-        // api.ws.debug(true);
-        api.connect(url, (newStatus) => {
-            if (newStatus === "open") wsOpen();
-            else status = "boom";
-        });
-    });
-
-    // Functions
-    async function wsOpen() {
-        lostConnection = "";
-        localStorage.setItem("lost-connection", lostConnection);
-
-        // On Websocket close
-        api.ws.onClose((event) => {
-            status = "close";
-            lostConnection = new Date().toLocaleString();
-            localStorage.setItem("lost-connection", lostConnection);
-        });
-
-        // Login
-        const token = localStorage.getItem("token");
-        const loginResponse = await user_v1.loginWithToken(token);
-        if (loginResponse === "ok") status = "authorized";
-        else status = "open";
-
-        // Errors
-        setTimeout(() => {
-            logger_v0.level.sub("error", rxError);
-            logger_v0.level.sub("warn", rxError);
-        }, 3000);
-    }
-    
-    function rxError(line) {
-        addTabActive({ name: "Logs" })
-        location.reload(true)
-    }
-
-    function addTabActive(tabAdded) {
-        setTimeout(() => {
-            let windowActiveIndex = $state.windows.findIndex(
-                (window) => window.id === $state.windowActiveId
-            );
-            if (
-                $state.windows[windowActiveIndex].state.tabs.some(
-                    (tab) => tab.name === tabAdded.name
-                )
-            ) {
-                $state.windows[windowActiveIndex].state.tabActive = tabAdded;
-            } else {
-                $state.windows[windowActiveIndex].state.tabs = [
-                    ...$state.windows[windowActiveIndex].state.tabs,
-                    tabAdded,
-                ];
-                $state.windows[windowActiveIndex].state.tabActive = tabAdded;
-            }
-        }, 1);
-    }
-
-    // Dynamic Variables
-    $: localStorage.setItem("status", status);
-    $: mainWindow = $state.windows.find((window) => window.parentId === 0);
-    $: console.log("status:", status);
+    import Debug from "./pages/Debug.svelte";
+    import Main from "./pages/Main.svelte";
+    import Split from "./components/Split.svelte";
 </script>
 
-
-{#if status === "startup"}
-    <article />
-{:else if status === "close"}
-    <article>
-        <h1>Lost Connection</h1>
-        <p>The connection to {url} was lost {lostConnection}</p>
-        <button on:click={() => location.reload()}>Reload</button>
-    </article>
-{:else if status === "open"}
-    <Login />
-{:else if status === "authorized" && mainWindow?.type === "tabs"}
-    <Tabs id={mainWindow.id} />
-{:else if status === "authorized" && mainWindow?.type === "split"}
+{#if $volatile.url.query?.debug === "true"}
     <Split
-        childrenIds={mainWindow?.childrenIds}
-        isVertical={mainWindow?.state.isVertical}
-    />
+        isResizable={true}
+        isVertical={false}
+        oneInit_percent={18}
+    >
+        <div slot="one">
+            <Debug />
+        </div>
+        <div slot="two">
+            <Main />
+        </div>
+    </Split>
+{:else}
+    <Main />
 {/if}
+
+<style>
+    div {
+        height: 100%;
+    }
+</style>
---
+++ src/startup.js
@@ -0,0 +1,67 @@
+import { ws, user_v1 } from "./api/api.js"
+import { volatile } from "./js/global-volatile-store.js"
+
+// State
+const url = "ws://192.168.1.1:4620"
+
+// Connect
+ws.connect(url, (status) => {
+    if (status === "open") onConnect()
+    else onFailedToConnect()
+})
+
+// Functions
+async function onConnect() {
+    volatile.update(val => {
+        val.apiStatus = "connected"
+        val.lostConnectionAt = ""
+        return val
+    })
+
+    // On Websocket close
+    ws.onClose(onClosed)
+
+    // Login
+    const token = localStorage.getItem("token")
+    const loginResponse = await user_v1.loginWithToken(token)
+    if (loginResponse === "ok") onLogin()
+    else onFailedToLogin()
+}
+
+function onClosed() {
+    volatile.update(val => {
+        val.apiStatus = "closed"
+        val.lostConnectionAt = new Date().toLocaleString()
+        return val
+    })
+}
+
+function onLogin() {
+    volatile.update(val => {
+        val.apiStatus = "authorized"
+        val.lostConnectionAt = ""
+        return val
+    })
+
+    user_v1.whoAmI.sub((user) => {
+        if (user.username) onWhoAmI(user)
+    })
+
+}
+
+function onWhoAmI(user) {
+    user.isAdmin = user.groups.some((group) => group === "admin")
+
+    volatile.update(val => {
+        val.user = user
+        return val
+    })
+}
+
+async function onFailedToConnect() {
+
+}
+
+async function onFailedToLogin() {
+
+}
